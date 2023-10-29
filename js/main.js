297ffc2dcc81e219f2b9ae0f8d78ee5845231386
zLilo <murilofm.social@gmail.com>
2023-10-29 19:22:34 -0300

    Game1Optimized

--- js/main.js
+++ js/main.js
@@ -1,247 +1,63 @@
-var turn = 1
-var text = document.getElementById('turn')
-var a1 = 0, a2 = 0, a3=0, b1=0, b2=0, b3=0, c1=0, c2=0, c3=0
-var xa=0, xb=0, xc=0, x1=0, x2=0, x3=0, xd1=0, xd2=0
-var oa=0, ob=0, oc=0, o1=0, o2=0, o3=0, od1=0, od2=0
+var turn = 1, i, a1=0, a2=0, a3=0, b1=0, b2=0, b3=0, c1=0, c2=0, c3=0
+var spaceId = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
+const marka1 = () => {i = 0; mark()}
+const marka2 = () => {i = 1; mark()}
+const marka3 = () => {i = 2; mark()}
+const markb1 = () => {i = 3; mark()}
+const markb2 = () => {i = 4; mark()}
+const markb3 = () => {i = 5; mark()}
+const markc1 = () => {i = 6; mark()}
+const markc2 = () => {i = 7; mark()}
+const markc3 = () => {i = 8; mark()}
 
-const marka1 = () => {
-    const position = document.getElementById('a1')
-    if(a1===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xa+=8
-            x1+=8
-            xd1+=8
-            a1++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            oa+=8
-            o1+=8
-            od1+=8
-            a1++
-        }
-    }
-    winner()
-}
-
-const markb1 = () => {
-    const position = document.getElementById('b1')
-    if(b1===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xb+=1
-            x1+=1
-            b1++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            ob+=1
-            o1+=1
-            b1++
-        }
-    }
-    winner()
-}
-const markc1 = () => {
-    const position = document.getElementById('c1')
-    if(c1===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xc+=6
-            x1+=6
-            xd2+=6
-            c1++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            oc+=6
-            o1+=6
-            od2+=6
-            c1++
-        }
-    }
-    winner()
-}
-const marka2 = () => {
-    const position = document.getElementById('a2')
-    if(a2===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xa+=3
-            x2+=3
-            a2++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            oa+=3
-            o2+=3
-            a2++
-        }
-    }
-    winner()
-}
-const markb2 = () => {
-    const position = document.getElementById('b2')
-    if(b2===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xb+=5
-            x2+=5
-            xd1+=5
-            xd2+=5
-            b2++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            ob+=5
-            o2+=5
-            od1+=5
-            od2+=5
-            b2++
-        }
-    }
-    winner()
-}
-const markc2 = () => {
-    const position = document.getElementById('c2')
-    if(c2===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xc+=7
-            x2+=7
-            c2++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            oc+=7
-            o2+=7
-            c2++
-        }
-    }
-    winner()
-}
-const marka3 = () => {
-    const position = document.getElementById('a3')
-    if(a3===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xa+=4
-            x3+=4
-            xd2+=4
-            a3++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            oa+=4
-            o3+=4
-            od2+=4
-            a3++
-        }
-    }
-    winner()
-}
-const markb3 = () => {
-    const position = document.getElementById('b3')
-    if(b3===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xb+=9
-            x3+=9
-            b3++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            ob+=9
-            o3+=9
-            b3++
-        }
-    }
-    winner()
-}
-const markc3 = () => {
-    const position = document.getElementById('c3')
-    if(c3===0){
-        if(turn === 1){
-            position.innerHTML = '<h3>X<h3>'
-            turn += 1
-            text.innerHTML = 'O'
-            xc+=2
-            x3+=2
-            xd1+=2
-            c3++
-        }
-        else{
-            position.innerHTML = '<h3>O<h3>'
-            turn -= 1
-            text.innerHTML = 'X'
-            oc+=2
-            o3+=2
-            od1+=2
-            c3++
+const mark = () => {
+    if(document.getElementById(spaceId[i]).innerHTML===' '){
+        if(turn===1){
+            document.getElementById(spaceId[i]).innerHTML = '<h3>X</h3>'
+            document.getElementById('turn').innerHTML ='O'
+            turn++
+        } else {
+            document.getElementById(spaceId[i]).innerHTML = '<h3>O</h3>'
+            document.getElementById('turn').innerHTML ='X'
+            turn--
         }
     }
     winner()
 }
 const winner = () => {
-    if(x1 === 15 || x2 === 15 || x3 === 15 || xa === 15 || xb===15 || xc===15 || xd1===15 || xd2 === 15){
-        const final = document.getElementById('game')
-        final.innerHTML =`
+    switch (i) {
+        case 0: turn===1 ? a1 = 8 : a1 = 800; break;
+        case 1: turn===1 ? a2 = 3 : a2 = 300; break;
+        case 2: turn===1 ? a3 = 4 : a3 = 400; break;
+        case 3: turn===1 ? b1 = 1 : b1 = 100; break;
+        case 4: turn===1 ? b2 = 5 : b2 = 500; break;
+        case 5: turn===1 ? b3 = 9 : b3 = 900; break;
+        case 6: turn===1 ? c1 = 6 : c1 = 600; break;
+        case 7: turn===1 ? c2 = 7 : c2 = 700; break;
+        default:turn===1 ? c3 = 2 : c3 = 200; break;
+    }
+    if (a1+a2+a3===15||b1+b2+b3===15||c1+c2+c3===15||a1+b1+c1===15||a2+b2+c2===15||a3+b3+c3===15||a1+b2+c3===15||c1+b2+a3===15){
+        document.getElementById('game').innerHTML =`
+        <div class="finalwindow">
+            <h2>Jogador com <span>O</span> ganhou!!</h2>
+            <img src="img/tobey-maguire-spider-man.gif" alt="tobey-maguire-spider-man">
+            <button id="playAgain" onclick="window.location.reload()">Jogar novamente</button>
+        </div>`
+    }
+    else if (a1+a2+a3===1500||b1+b2+b3===1500||c1+c2+c3===1500||a1+b1+c1===1500||a2+b2+c2===1500||a3+b3+c3===1500||a1+b2+c3===1500||c1+b2+a3===1500){
+        document.getElementById('game').innerHTML =`
         <div class="finalwindow">
             <h2>Jogador com <span>X</span> ganhou!!</h2>
             <img src="img/tobey-maguire-spider-man.gif" alt="tobey-maguire-spider-man">
             <button id="playAgain" onclick="window.location.reload()">Jogar novamente</button>
         </div>`
     }
-    else if(o1 === 15 || o2 === 15 || o3 === 15 || oa === 15 || ob===15 || oc===15 || od1===15 || od2 === 15){
-        const final = document.getElementById('game')
-         final.innerHTML =`
-         <div class="finalwindow">
-            <h2>Jogador com <span>O</span> ganhou!!</h2>
-            <img src="img/tobey-maguire-spider-man.gif" alt="tobey-maguire-spider-man">
-            <button id="playAgain" onclick="window.location.reload()">Jogar novamente</button>
-         </div>`
-    }
-    if(a1+a2+a3+b1+b2+b3+c1+c2+c3===9){
-        const final = document.getElementById('game')
-         final.innerHTML =`
+    else if(a1!=0 && a2!=0 && a3!=0 && b1!=0 && b2!=0 && b3!=0 && c1!=0 && c2!=0 && c3!=0){
+        document.getElementById('game').innerHTML =`
          <div class="finalwindow">
             <h2 class="tie">Deu <span>Velha</span>!!!</h2>
             <img src="img/velha.gif" alt="tobey-maguire-spider-man">
             <button id="playAgain" onclick="window.location.reload()">Jogar novamente</button>
          </div>`
     }
-    console.log(a1+a2+a3+b1+b2+b3+c1+c2+c3===9)
 }
